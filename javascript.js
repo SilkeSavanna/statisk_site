49e76de3222a6cf129e66719f9a9d64cdb546af0
SilkeSavanna <sihe0002@stud.kea.dk>
2024-02-06 21:03:47 +0100

    product_javascript

--- javascript.js
+++ javascript.js
@@ -1,22 +1,3 @@
-// window.addEventListener("load", sidenVises);
-
-// function sidenVises() {
-//   console.log("sidenVises");
-
-//   document.querySelectorAll(".toggle-btn").forEach((btn) => {
-//     // Der tilføjes en eventlistener til hvert af disse elementer, der venter på et klik.
-//     btn.addEventListener("click", function () {
-//       //Ved klik på et af disse elementer, finder koden forældreelementet til det klikkede element og gemmer det i variablen "produkt".
-//       var produkt = this.parentElement;
-
-//       //Der tilføjes eller fjernes klassen "open" fra forældreelementet. Hvis "open"-klassen ikke er til stede, tilføjer den klassen, og hvis den allerede er til stede, fjerner den klassen.
-//       produkt.classList.toggle("open");
-
-//       // Samlet set tillader denne kode, at når der klikkes på elementer med klassen "toggle-btn", ændres klassen "open" for det overordnede element, hvilket potentielt kan aktivere eller deaktivere visse visuelle eller funktionelle egenskaber på det pågældende element eller dets børneelementer.
-//     });
-//   });
-// }
-
 window.addEventListener("DOMContentLoaded", init);
 
 const productURL = "https://kea-alt-del.dk/t7/api/products";
@@ -50,13 +31,27 @@ function showProducts(productJSON) {
   productJSON.forEach((product) => {
     console.log("Product", product);
     productClone = productTemplate.cloneNode(true).content;
-    productClone.querySelector("a").href = `produkt.html?id=${product.id}`;
+    productClone.querySelector(
+      ".produkt_link"
+    ).href = `produkt.html?id=${product.id}`;
+
     productClone.querySelector(".produkt_image");
     productClone.querySelector(".produkt_name").textContent =
       product.productdisplayname;
     productClone.querySelector(".produkt_tagline").textContent =
       product.articletype;
     productClone.querySelector(".produkt_price").textContent = product.price;
+
+    productClone.querySelector(".produkt_discount_data").textContent =
+      product.discount;
+
+    if (product.discount) {
+      productClone.querySelector(".produkt_discount").classList.remove("hide");
+    }
+
+    if (product.soldout >= 1) {
+      productClone.querySelector(".produkt_soldout").classList.remove("hide");
+    }
 
     productContainer.appendChild(productClone);
   });
